1bf4c2162c4eae4e78a37587486904436d409af8
bhanumetal <101553709+bhanumetal@users.noreply.github.com>
2025-12-16 07:36:48 +0530

    first commit
    - first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+// Simple GET Route 1
+app.get("/hello", (req, res) => {
+  res.json({ message: "Hello World!" });
+});
+
+// Simple GET Route 2
+app.get("/info", (req, res) => {
+  res.json({ app: "My Express API", version: "1.0" });
+});
+
+// Start Server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
